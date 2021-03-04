2481f4f05540c7093f11d4727a99fbaf9371ed30
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-03-04 12:12:05 +0100

    Add remaining props to components root element

--- src/components/form/Checkbox.js
+++ src/components/form/Checkbox.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import "./Checkbox.css";
 
-export default function Checkbox({text, checked=false}) {
+export default function Checkbox({ text, checked = false }) {
     const [value, setValue] = useState(checked);
+
     return (
         <label class="control control--checkbox mb-0">
-            <span class="caption">{ text }</span>
-            <input type="checkbox" value={value} defaultChecked={value} onClick={e=>setValue(!value)}/>
+            <span class="caption">{text}</span>
+            <input
+                type="checkbox"
+                value={value}
+                defaultChecked={value}
+                onClick={(e) => setValue(!value)}
+            />
             <div class="control__indicator"></div>
         </label>
-    )
+    );
 }
--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,13 +1,29 @@
-import {
-    FormGroup,
-    FormControl,
-} from "react-bootstrap";
+import { useState } from "react";
+import { FormGroup, FormControl } from "react-bootstrap";
 
-export default function Input({name, type}) {
+export default function Input({
+    name,
+    type,
+    defaultValue = "",
+    className,
+    ...props
+}) {
+    const [value, setValue] = useState(defaultValue);
+
     return (
-        <FormGroup className="first">
-            <label htmlFor={{ name }}>{ name }</label>
-            <FormControl type={{ type }} id={{ name }} required />
+        <FormGroup
+            className={`${value != "" && "field--not-empty"} ${className}`}
+            {...props}
+        >
+            <label htmlFor={name}>{name}</label>
+            <FormControl
+                defaultValue={value}
+                value={value}
+                type={type}
+                id={name}
+                onChange={(e) => setValue(e.target.value)}
+                required
+            />
         </FormGroup>
-    )
+    );
 }
